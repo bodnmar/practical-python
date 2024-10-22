ff2817b585b49c887a5660c5e3b10529cc27f5ff
bodnmar <96832778+bodnmar@users.noreply.github.com>
2024-10-22 09:56:10 -0400

    Update bounce.py
    Wrote program

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+height = 100.0
+bounce_number =1
+
+while bounce_number < 11:
+    height = .6 * height
+    print(bounce_number, round(height,4))
+    bounce_number = bounce_number + 1
